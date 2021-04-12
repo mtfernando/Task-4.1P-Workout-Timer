c1a1e47e88f7c27be502d54adae26893b0f1033a
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-04-12 10:29:48 +1000

    Chronometer added
    Chronometer added with functionality to start, pause, and reset the stopwatch.

--- app/src/main/java/com/example/task41pworkouttimer/MainActivity.java
+++ app/src/main/java/com/example/task41pworkouttimer/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.task41pworkouttimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer2);
+    }
+
+    public void startTimer(View view){
+        if(!running){
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void pauseTimer(View view){
+        if(running){
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void stopTimer(View view){
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+        running = false;
     }
 }
