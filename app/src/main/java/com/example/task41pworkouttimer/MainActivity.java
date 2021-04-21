4daf3ffa9bb293d4c09b5bf60a057b4d36ddd261
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-04-21 18:16:37 +1000

    savedInstanceState for Chronometer - PauseOffset Bug Fix Required
    PauseOffset needs to update when the screen is rotated. This bug will be fixed in a future commit.

--- app/src/main/java/com/example/task41pworkouttimer/MainActivity.java
+++ app/src/main/java/com/example/task41pworkouttimer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.task41pworkouttimer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Chronometer;
@@ -10,14 +12,31 @@ import android.widget.Chronometer;
 public class MainActivity extends AppCompatActivity {
 
     private Chronometer chronometer;
-    private long pauseOffset;
-    private boolean running;
+    private long pauseOffset=0;
+    private boolean running=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         chronometer = findViewById(R.id.chronometer2);
+
+        if(savedInstanceState!=null){
+            chronometer.setBase(savedInstanceState.getLong("ChronoTime") - savedInstanceState.getLong("PauseOffset"));
+            running = savedInstanceState.getBoolean("running");
+            if(running){
+                chronometer.start();
+            }
+            System.out.println("onCreate is running!");
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putLong("ChronoTime", chronometer.getBase());
+        savedInstanceState.putFloat("PauseOffset", pauseOffset);
+        savedInstanceState.putBoolean("running", running);
     }
 
     public void startTimer(View view){
@@ -37,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void stopTimer(View view){
+        chronometer.stop();
         chronometer.setBase(SystemClock.elapsedRealtime());
         pauseOffset = 0;
         running = false;
