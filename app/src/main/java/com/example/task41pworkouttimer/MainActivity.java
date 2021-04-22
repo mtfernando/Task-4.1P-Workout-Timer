40a91c797d8ae0ec866275a911946cdc7116a01f
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-04-23 08:32:39 +1000

    SharedPreferences added
    Application saves and displays previous workout details

--- app/src/main/java/com/example/task41pworkouttimer/MainActivity.java
+++ app/src/main/java/com/example/task41pworkouttimer/MainActivity.java
@@ -3,24 +3,38 @@ package com.example.task41pworkouttimer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Chronometer chronometer;
+    private TextView workoutTextView;
+    private TextView workoutTypeEditText;
     private long pauseOffset;
     private boolean running=false;
     private long chronotime;
+    private SharedPreferences sp;
+    public String workoutTime;
+    public String workoutType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sp = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
+
         chronometer = findViewById(R.id.chronometer2);
+        workoutTextView = findViewById(R.id.workoutTextView);
+        workoutTypeEditText = findViewById(R.id.editTextWorkoutType);
+
+        updateWorkoutDetails();
 
         if(savedInstanceState!=null){
 
@@ -46,9 +60,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChronometerTick(Chronometer chronometer)
             {
-                chronotime = SystemClock.elapsedRealtime() - chronometer.getBase();;
+                chronotime = SystemClock.elapsedRealtime() - chronometer.getBase();
             }
         });
+    }
+
+    //This function updates the text view with your previous workout details.
+    public void updateWorkoutDetails(){
+        System.out.println("Update function is running");
+
+        workoutTime = sp.getString("workoutTime", "");
+        workoutType = sp.getString("workoutType", "");
+
+        System.out.println(workoutTime);
+        System.out.println(workoutType);
+        if(!workoutTime.equals("")) {
+            System.out.println("Loop 1");
+            if(!workoutType.equals("")){
+                System.out.println("Loop 2");
+                workoutTextView.setText("You spent " + workoutTime + " on " + workoutType + " last time.");
+            }
+            else{
+                workoutTextView.setText("You spent " + workoutTime + " on your workout last time.");
+            }
+        }
     }
 
     public void onSaveInstanceState(Bundle savedInstanceState){
@@ -90,6 +125,35 @@ public class MainActivity extends AppCompatActivity {
     public void stopTimer(View view){
         chronometer.stop();
 
+        //Chronometer time to a string
+        int mins = (int)(chronotime/1000/60);
+        int secs = (int)((chronotime/1000)-(mins*60));
+        String minsStr = String.valueOf(mins);
+        String secsStr = String.valueOf(secs);
+
+        //Padding Zeros
+        if(mins<10){
+            minsStr = ("0"+minsStr);
+        }
+        if(secs<10){
+            secsStr = ("0"+secsStr);
+        }
+
+        String timeStr = (minsStr+":"+secsStr);
+        System.out.println(timeStr);
+
+        //Only updating shared preferences if an actual workout was done.
+        //Avoids accidentail erasing of previous workout details by clicking the stop button.
+        System.out.println(chronotime);
+        if(chronotime>0){
+            SharedPreferences.Editor workoutEdit = sp.edit();
+            workoutEdit.putString("workoutTime", timeStr);
+            workoutEdit.putString("workoutType", workoutTypeEditText.getText().toString());
+
+            workoutEdit.commit();
+            updateWorkoutDetails();
+        }
+
         //Reset chronometer base to current time - 00:00
         chronometer.setBase(SystemClock.elapsedRealtime());
 
@@ -98,6 +162,6 @@ public class MainActivity extends AppCompatActivity {
         chronotime = 0;
         running = false;
 
-        //TODO - UPDATE SHARED PREFERENCES WITH WORKOUT DETAILS
+        Toast.makeText(getApplicationContext(), "Stopwatch has been reset!", Toast.LENGTH_SHORT).show();
     }
 }
